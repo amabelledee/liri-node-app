744e71ef2256d074c23adf96e9eae7356cc1560e
Hatshepsut <amabelle.delacruz322@gmail.com>
2019-01-15 20:33:37 -0800

    removed keys

--- liri.js
+++ liri.js
@@ -6,8 +6,7 @@ var dotenv = require('dotenv').config()
 var Spotify = require("node-spotify-api")
 
 var spotify = new Spotify({
-    id: d9d4ca62f40c454282e567a17d8890ed,
-    secret: e3f5282e396242e682120de203bc9ad3
+
   });
    
   spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
